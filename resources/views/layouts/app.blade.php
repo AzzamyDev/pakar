c2d0ddccd29435929c78211ab8bd2ddb483d0ef4
azzamyde <azzamyde@azzamydev.my.id>
2022-01-21 22:33:17 +0700

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -11,7 +12,7 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
-    
+
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -20,94 +21,109 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-{{-- summernote --}}
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" ></script>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    {{-- summernote --}}
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
-   </head>
+</head>
+
 <body>
     <div id="app">
         <div class="navbar navbar-light bg-white">
             <div class="container">
-            <a class="navbar-brand me-3 text-primary" href="{{ url('/') }}">
+                <a class="navbar-brand me-3 text-primary" href="{{ url('/') }}">
                     Aplikasi Pakar Illnes
-            </a>
+                </a>
             </div>
         </div>
         @guest
-            
+
         @else
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+            <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+                <div class="container">
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                        aria-controls="navbarSupportedContent" aria-expanded="false"
+                        aria-label="{{ __('Toggle navigation') }}">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    @guest
-                    <h5 class="text-primary">Selamat Datang</h5>
-                    @else
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item me-3">
-                            <a class="nav-link @if (Route::is('diseases.index')) active @endif" aria-current="page" href="{{route('diseases.index')}}"><span><i class="fas fa-bacteria"></i></span> Penyakit</a>
-                        </li>
-                        <li class="nav-item me-3">
-                            <a class="nav-link @if (Route::is('indications.index')) active @endif" aria-current="page" href="{{route('indications.index')}}"><span><i class="fab fa-creative-commons-sampling"></i></span> Gejala</a>
-                        </li>
-                        @role('psikolog')
-
-                        @else
-                        <li class="nav-item me-3">
-                            <a class="nav-link @if (Route::is('psikologs.index')) active @endif" aria-current="page" href="{{route('psikologs.index')}}"><span><i class="fas fa-users"></i></span> Psikolog</a>
-                        </li>
-                        <li class="nav-item me-3">
-                            <a class="nav-link @if (Route::is('users.index')) active @endif" aria-current="page" href="{{route('users.index')}}"><span><i class="fas fa-user-circle"></i></span> Pengguna</a>
-                        </li>
-                        @endrole
-                        <li class="nav-item me-3">
-                            <a class="nav-link @if (Route::is('record')) active @endif" aria-current="page" href="{{route('record')}}"><span><i class="fas fa-notes-medical"></i></span> Track Record Users</a>
-                        </li>
-                    </ul>
-                    @endguest
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <!-- Left Side Of Navbar -->
                         @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="btn btn-sm btn-primary" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
-
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
+                            <h5 class="text-primary">Selamat Datang</h5>
                         @else
-                        <li class="nav-item me-4">
-                            <h6 class="text-secondary">{{ Auth::user()->name }}</h6>
-                        </li>
-                        <li class="nav-item">
-                        <a class="btn btn-sm btn-danger" href="{{ route('logout') }}"
-                                    onclick="event.preventDefault();
-                                                    document.getElementById('logout-form').submit();">
-                                    <i class="fas fa-sign-out-alt"></i> {{ __('Logout') }}
-                                </a>
+                            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                                <li class="nav-item me-3">
+                                    <a class="nav-link @if (Route::is('diseases.index')) active @endif" aria-current="page"
+                                        href="{{ route('diseases.index') }}"><span><i class="fas fa-bacteria"></i></span>
+                                        Penyakit</a>
+                                </li>
+                                <li class="nav-item me-3">
+                                    <a class="nav-link @if (Route::is('indications.index')) active @endif" aria-current="page"
+                                        href="{{ route('indications.index') }}"><span><i
+                                                class="fab fa-creative-commons-sampling"></i></span> Gejala</a>
+                                </li>
+                                @role('admin')
+                                    <li class="nav-item me-3">
+                                        <a class="nav-link @if (Route::is('psikologs.index')) active @endif" aria-current="page"
+                                            href="{{ route('psikologs.index') }}"><span><i class="fas fa-users"></i></span>
+                                            Psikolog</a>
+                                    </li>
+                                    <li class="nav-item me-3">
+                                        <a class="nav-link @if (Route::is('users.index')) active @endif" aria-current="page"
+                                            href="{{ route('users.index') }}"><span><i class="fas fa-user-circle"></i></span>
+                                            Pengguna</a>
+                                    </li>
+                                @else
 
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
-                        </li>
-                            
+                                @endrole
+                                <li class="nav-item me-3">
+                                    <a class="nav-link @if (Route::is('record')) active @endif" aria-current="page"
+                                        href="{{ route('record') }}"><span><i class="fas fa-notes-medical"></i></span> Track
+                                        Record Users</a>
+                                </li>
+                            </ul>
                         @endguest
-                    </ul>
+                        <!-- Right Side Of Navbar -->
+                        <ul class="navbar-nav ml-auto">
+                            <!-- Authentication Links -->
+                            @guest
+                                @if (Route::has('login'))
+                                    <li class="nav-item">
+                                        <a class="btn btn-sm btn-primary"
+                                            href="{{ route('login') }}">{{ __('Login') }}</a>
+                                    </li>
+                                @endif
+
+                                @if (Route::has('register'))
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    </li>
+                                @endif
+                            @else
+                                <li class="nav-item me-4">
+                                    <h6 class="text-secondary">{{ Auth::user()->name }}</h6>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="btn btn-sm btn-danger" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                            document.getElementById('logout-form').submit();">
+                                        <i class="fas fa-sign-out-alt"></i> {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                        class="d-none">
+                                        @csrf
+                                    </form>
+                                </li>
+
+                            @endguest
+                        </ul>
+                    </div>
                 </div>
-            </div>
-        </nav>
+            </nav>
         @endguest
         <main class="py-4">
             @yield('content')
@@ -115,10 +131,15 @@
     </div>
     <!-- Option 1: Bootstrap Bundle with Popper -->
     {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script> --}}
-     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-   
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
+        integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
+        integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous">
+    </script>
+
     @stack('cari')
     @stack('summernote')
 </body>
+
 </html>
